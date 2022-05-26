750866f361deb6184a95ab0127c7e420fc7d3bfa
Aito Hideki <aito.hideki@gmail.com>
2022-05-26 16:50:09 -0400

    feat(hero): updated hero section

--- components/HeroOpSection.vue
+++ components/HeroOpSection.vue
@@ -1,122 +1,40 @@
 <template>
-	<div class="relative bg-gray-800 overflow-hidden">
-		<div class="hidden sm:block sm:absolute sm:inset-0" aria-hidden="true">
-			<svg
-				class="
-					absolute
-					bottom-0
-					right-0
-					transform
-					translate-x-1/2
-					mb-48
-					text-gray-700
-					lg:top-0 lg:mt-28 lg:mb-0
-					xl:transform-none xl:translate-x-0
-				"
-				width="364"
-				height="384"
-				viewBox="0 0 364 384"
-				fill="none">
-				<defs>
-					<pattern
-						id="eab71dd9-9d7a-47bd-8044-256344ee00d0"
-						x="0"
-						y="0"
-						width="20"
-						height="20"
-						patternUnits="userSpaceOnUse">
-						<rect x="0" y="0" width="4" height="4" fill="currentColor" />
-					</pattern>
-				</defs>
-				<rect
-					width="364"
-					height="384"
-					fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)" />
-			</svg>
-		</div>
-		<div class="relative pt-6 pb-16 sm:pb-24">
-			<main class="mt-16 sm:mt-24">
-				<div class="mx-auto max-w-7xl">
-					<div class="lg:grid lg:grid-cols-12 lg:gap-8">
-						<div
-							class="
-								px-4
-								sm:px-6 sm:text-center
-								md:max-w-2xl md:mx-auto
-								lg:col-span-6 lg:text-left lg:flex lg:items-center
-							">
-							<div>
-								<h1
-									class="
-										mt-4
-										text-4xl
-										tracking-tight
-										font-extrabold
-										text-white
-										sm:mt-5 sm:leading-none
-										lg:mt-6 lg:text-5xl
-										xl:text-6xl
-									">
-									<span class="md:block">WELCOME TO</span>
-									{{ ' ' }}
-									<span class="text-indigo-400 md:block">NFT TEMPLATE</span>
-								</h1>
-								<p
-									class="
-										mt-3
-										text-base text-gray-300
-										sm:mt-5 sm:text-xl
-										lg:text-lg
-										xl:text-xl
-									">
-									Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-									cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua
-									ad ad non deserunt sunt.
-								</p>
-								<p
-									class="
-										mt-8
-										text-sm text-white
-										uppercase
-										tracking-wide
-										font-semibold
-										sm:mt-10
-									">
-									Featured ON:
-								</p>
-								<div class="mt-5 w-full sm:mx-auto sm:max-w-lg lg:ml-0">
-									<div class="flex flex-wrap items-start justify-between">
-										<div class="flex justify-center px-1">
-											<img
-												class="h-9 sm:h-10"
-												src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
-												alt="Tuple" />
-										</div>
-										<div class="flex justify-center px-1">
-											<img
-												class="h-9 sm:h-10"
-												src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
-												alt="Workcation" />
-										</div>
-										<div class="flex justify-center px-1">
-											<img
-												class="h-9 sm:h-10"
-												src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
-												alt="StaticKit" />
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="mt-16 sm:mt-24 lg:mt-0 lg:col-span-5 p-8">
-							<img
-								class="shadow-gray-700 object-cover object-center rounded-3xl shadow-lg"
-								src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=980:*"
-								alt="" />
-						</div>
-					</div>
+	<div class="container relative overflow-hidden px-8 pt-60 pb-24 mx-auto">
+		<div class="flex items-start justify-start mb-32">
+			<div class="w-2/3 uppercase">
+				<div class="text-light text-2xl font-bold mb-2">
+					Twinz NFT
 				</div>
-			</main>
+				<div class="text-7xl font-bold mb-8">
+					Come play with our family,
+					<span class="text-primary">Twin</span>ning is winning!
+				</div>
+				<div class="flex">
+					<a class="flex bg-primary font-bold border border-solid border-primary mr-8 px-9 py-5">
+						Learn more
+					</a>
+					<a class="flex text-primary font-bold border border-solid border-primary px-9 py-5">
+						Join to Discord
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="flex text-light pl-6 pr-12">
+			<div class="border-l-4 border-l-primary w-1/3 px-6 py-2">
+				<div class="max-w-2/3">
+					TwinZ is a collection of 10,000 digital individuals living as NFTs
+				</div>
+			</div>
+			<div class="border-l-4 border-l-primary w-1/3 px-6 py-2">
+				<div class="max-w-2/3">
+					5 different societies with completely different visual atmospheres
+				</div>
+			</div>
+			<div class="border-l-4 border-l-primary w-1/3 px-6 py-2">
+				<div class="max-w-2/3">
+					Member of TwinZ are eligible for monthly airdrops of "Royalty Rewards".
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
