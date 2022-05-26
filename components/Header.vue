f907730bee842fb01a884549da84353411d81c22
Aito Hideki <aito.hideki@gmail.com>
2022-05-26 16:01:49 -0400

    feat(header): updated header

--- components/Header.vue
+++ components/Header.vue
@@ -1,39 +1,57 @@
 <template>
   <header
-    :class="`bg-black ${scrollY < 1 ? 'sm:bg-transparent' : ''}  flex justify-center items-baseline fixed px-3 py-3 w-full z-50`"
+    :class="`bg-dark ${scrollY < 1 ? 'sm:bg-transparent' : ''} flex justify-center items-center fixed px-8 py-5 w-full z-50`"
   >
     <ConnectModal/>
 
-    <div class="flex justify-between items-baseline w-full sm:w-11/12">
-      <Logo/>
+    <div class="flex justify-between items-center w-full">
+      <Logo />
       <!-- Desktop nav -->
       <nav class="hidden lg:flex">
-        <ul class="flex text-white items-center">
+        <ul class="flex text-white text-sm font-bold uppercase items-center">
           <li class="mr-8">
-            <a class="text-lg xl:text-2xl" href="/#presentation-section">Presentation</a>
+            <a href="/#story">Story</a>
           </li>
           <li class="mr-8">
-            <a class="text-lg xl:text-2xl" href="/#roadmap-section">Road Map</a>
+            <a href="/#project">About Project</a>
           </li>
           <li class="mr-8">
-            <a class="text-lg xl:text-2xl" href="/#team-section">Our Team</a>
+            <a href="/#utilities">Utilities</a>
           </li>
           <li class="mr-8">
-            <a class="text-lg xl:text-2xl" href="/#faq-section">FAQ</a>
+            <a href="/#parties">Parties</a>
+          </li>
+          <li class="mr-8">
+            <a href="/#roadmap">Roadmap</a>
+          </li>
+          <li class="mr-8">
+            <a href="/#team">Team</a>
+          </li>
+          <li class="mr-8">
+            <a href="/#faq">FAQ</a>
           </li>
           <li>
-            <a class="text-lg xl:text-2xl" href="/whitelist">Whitelist</a>
+            <a href="/#community">Community</a>
           </li>
         </ul>
       </nav>
-      <ConnectButton/>
+      <div class="flex">
+        <a class="flex items-center justify-center w-11 h-11 bg-primary mr-5">
+          <i class="fab fa-twitter text-2xl" />
+        </a>
+        <a class="flex items-center justify-center w-11 h-11 bg-primary">
+          <i class="fab fa-discord text-2xl" />
+        </a>
+      </div>
+      <!-- <ConnectButton/> -->
       <!-- Mobile menu -->
       <div class="flex lg:hidden justify-end">
         <button
-          class="text-white z-20 "
+          class="text-white z-20 mr-4"
           @click="toggleNavigator"
-          
-        ></button>
+        >
+          <i class="text-2xl fas fa-bars" />
+        </button>
         <transition name="scale">
           <nav
             v-if="isNavigatorOpen"
@@ -44,20 +62,29 @@
             }"
           >
             <ul class="flex flex-col text-white text-center">
-              <li class="mb-3">
-                <a href="/#presentation-section">The Forest</a>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#story">Story</a>
               </li>
-              <li class="mb-3">
-                <a href="/#roadmap-section">Road Map</a>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#project">About Project</a>
               </li>
-              <li class="mb-3">
-                <a href="/#team-section">Our Team</a>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#utilities">Utilities</a>
               </li>
-              <li class="mb-3">
-                <a href="/#faq-section">FAQ</a>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#parties">Parties</a>
               </li>
-              <li>
-                <a href="/whitelist">Whitelist</a>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#roadmap">Roadmap</a>
+              </li>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#team">Team</a>
+              </li>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#faq">FAQ</a>
+              </li>
+              <li class="mb-3" @click="toggleNavigator">
+                <a href="/#community">Community</a>
               </li>
               <li class="mt-8">
                 <a href="https://discord.com/invite/" class="discord">
