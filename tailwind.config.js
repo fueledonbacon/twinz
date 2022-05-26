9f5f753fa046588276b1bd3cb13d5779edac905d
Aito Hideki <aito.hideki@gmail.com>
2022-05-26 16:00:45 -0400

    style(palette): updated palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      colors: {
+        primary: '#F01E03',
+        dark: '#222222'
+      }
       // add modifications to styling classes here
     },
   },
