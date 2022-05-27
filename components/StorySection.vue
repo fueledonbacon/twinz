9cfaeb3bac4cde1ab75f2b8dddffa826785391c9
Aito Hideki <aito.hideki@gmail.com>
2022-05-26 23:07:17 -0400

    feat(story): updated story section background

--- components/StorySection.vue
+++ components/StorySection.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-		<div class="bg-[url(@/assets/images/about-bg.jpg)] bg-cover bg-no-repeat">
-			<div class="container mx-auto px-8 pt-24 xl:pt-32">
+		<div class="bg-hero bg-cover bg-no-repeat bg-bottom">
+			<div class="container mx-auto px-8 pt-24 xl:py-32">
 				<div class="mb-2 text-xl">物語</div>
 				<div class="mb-10 text-6xl font-bold md:text-8xl">
 					TWIN<span class="text-primary">Z</span>
@@ -34,8 +34,6 @@
 					</div>
 				</div>
 			</div>
-
-			<div class="h-24 bg-gradient-to-t from-dark to-transparent xl:h-32" />
 		</div>
 
 		<div class="container mx-auto px-8">
