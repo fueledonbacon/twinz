31fdff2acaac9dd6b701b6d385138647fea62ee0
Aito Hideki <aito.hideki@gmail.com>
2022-05-27 22:51:21 -0400

    feat(footer): updated footer section

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,44 +1,80 @@
 <template>
-	<footer class="container mx-auto px-8 pt-24 pb-12">
-		<div class="text-2xl font-bold uppercase text-center mb-16 max-w-[720px] mx-auto">
+	<footer
+		id="footer"
+		class="container mx-auto px-8 pt-24 pb-12">
+		<div
+			class="mx-auto mb-16 max-w-[720px] text-center text-2xl font-bold uppercase">
 			The <span class="text-primary">Twin</span>Zverse is one of the most dynamic,
 			unique, and loyal <span class="text-primary">families</span> you could ever
 			imagine. Come join our family and enjoy with us because
 			<span class="text-primary">Twinning is Winning</span>!
 		</div>
-    <div class="flex justify-center mb-20">
-      <a class="mr-5 flex h-11 w-11 items-center justify-center bg-primary">
-        <i class="fab fa-twitter text-2xl" />
-      </a>
-      <a class="flex h-11 w-11 items-center justify-center bg-primary">
-        <i class="fab fa-discord text-2xl" />
-      </a>
-    </div>
-    <ul class="flex flex-wrap items-center justify-center text-sm font-bold uppercase text-white">
-      <li class="inline-block mr-4 xl:mr-8 2xl:mr-12 my-2">
-        <a class="whitespace-nowrap" href="/#story">Story</a>
-      </li>
-      <li class="inline-block mr-4 xl:mr-8 2xl:mr-12 my-2">
-        <a class="whitespace-nowrap" href="/#project">About Project</a>
-      </li>
-      <li class="inline-block mr-4 xl:mr-8 2xl:mr-12 my-2">
-        <a class="whitespace-nowrap" href="/#utilities">Utilities</a>
-      </li>
-      <li class="inline-block mr-4 xl:mr-8 2xl:mr-12 my-2">
-        <a class="whitespace-nowrap" href="/#rarities">Rarities</a>
-      </li>
-      <li class="inline-block mr-4 xl:mr-8 2xl:mr-12 my-2">
-        <a class="whitespace-nowrap" href="/#roadmap">Roadmap</a>
-      </li>
-      <li class="inline-block mr-4 xl:mr-8 2xl:mr-12 my-2">
-        <a class="whitespace-nowrap" href="/#team">Team</a>
-      </li>
-      <li class="inline-block mr-4 xl:mr-8 2xl:mr-12 my-2">
-        <a class="whitespace-nowrap" href="/#faq">FAQ</a>
-      </li>
-      <li class="inline-block my-2">
-        <a class="whitespace-nowrap" href="/#community">Community</a>
-      </li>
-    </ul>
+		<div class="mb-20 flex justify-center">
+			<a class="mr-5 flex h-11 w-11 items-center justify-center bg-primary">
+				<i class="fab fa-twitter text-2xl" />
+			</a>
+			<a class="flex h-11 w-11 items-center justify-center bg-primary">
+				<i class="fab fa-discord text-2xl" />
+			</a>
+		</div>
+		<ul
+			class="flex flex-wrap items-center justify-center text-sm font-bold uppercase text-white">
+			<li class="my-2 mr-4 inline-block xl:mr-8 2xl:mr-12">
+				<a
+					class="whitespace-nowrap"
+					href="/#story"
+					>Story</a
+				>
+			</li>
+			<li class="my-2 mr-4 inline-block xl:mr-8 2xl:mr-12">
+				<a
+					class="whitespace-nowrap"
+					href="/#project"
+					>About Project</a
+				>
+			</li>
+			<li class="my-2 mr-4 inline-block xl:mr-8 2xl:mr-12">
+				<a
+					class="whitespace-nowrap"
+					href="/#utilities"
+					>Utilities</a
+				>
+			</li>
+			<li class="my-2 mr-4 inline-block xl:mr-8 2xl:mr-12">
+				<a
+					class="whitespace-nowrap"
+					href="/#rarities"
+					>Rarities</a
+				>
+			</li>
+			<li class="my-2 mr-4 inline-block xl:mr-8 2xl:mr-12">
+				<a
+					class="whitespace-nowrap"
+					href="/#roadmap"
+					>Roadmap</a
+				>
+			</li>
+			<li class="my-2 mr-4 inline-block xl:mr-8 2xl:mr-12">
+				<a
+					class="whitespace-nowrap"
+					href="/#team"
+					>Team</a
+				>
+			</li>
+			<li class="my-2 mr-4 inline-block xl:mr-8 2xl:mr-12">
+				<a
+					class="whitespace-nowrap"
+					href="/#faq"
+					>FAQ</a
+				>
+			</li>
+			<li class="my-2 inline-block">
+				<a
+					class="whitespace-nowrap"
+					href="/#community"
+					>Community</a
+				>
+			</li>
+		</ul>
 	</footer>
 </template>
