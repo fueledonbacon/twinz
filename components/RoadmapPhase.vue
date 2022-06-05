40c853d5e3de92321da6a2378f6d72fb73d08346
Aito Hideki <aito.hideki@gmail.com>
2022-06-05 12:46:32 -0400

    fix(feedback): feedback fixes

---
+++ components/RoadmapPhase.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    ref="block"
+    class="relative flex flex-col rounded-tr-3xl px-8 pt-4 pb-6 sm:rounded-tr-[150px] sm:px-24 sm:pt-16 sm:pb-20 md:flex-row duration-300"
+    :class="{
+      'bg-primary': active,
+      'bg-neutral-400/10 backdrop-blur-md': !active,
+    }">
+    <div class="mb-5 w-full text-xl font-bold sm:text-4xl md:w-2/5">
+      <div class="lg:w-2/3">
+        <span class="inline-block whitespace-nowrap">
+          {{ data.title }} -&nbsp;
+        </span>
+        <span class="inline-block whitespace-nowrap"> {{ data.period }} </span>
+      </div>
+    </div>
+    <div class="font-lato w-full text-sm sm:text-lg md:w-3/5">
+      {{ data.todo }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    active: false
+  }),
+  props: {
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  mounted () {
+    document.addEventListener('scroll', this.checkScrolling)
+  },
+  beforeUnmount () {
+    document.removeEventListener('scroll', this.checkScrolling)
+  },
+  methods: {
+    checkScrolling () {
+      if (!this.$refs.block) { return }
+
+      const { top, bottom } = this.$refs.block.getBoundingClientRect()
+      this.active = (top < 150) && (bottom > 100)
+    }
+  }
+}
+</script>
