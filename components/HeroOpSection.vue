6bda3b91d365b5f40c9ebb9d36e08c9cb539dcd4
Ryan Cwynar <ryan@fueledonbacon.com>
2022-09-28 17:59:40 -0500

    add minting toasts

--- components/HeroOpSection.vue
+++ components/HeroOpSection.vue
@@ -1,30 +1,17 @@
 <template>
 	<div class="relative">
-		<img
-			class="absolute top-10 right-0 w-1/3 max-w-[400px] md:z-10"
-			src="@/assets/images/hero-1.png"
+		<img class="absolute top-10 right-0 w-1/3 max-w-[400px] md:z-10" src="@/assets/images/hero-1.png"
 			alt="Sakura" />
-		<img
-			class="absolute bottom-[15%] left-0 w-1/5 max-w-[300px]"
-			src="@/assets/images/hero-2.png"
-			alt="Sakura" />
+		<img class="absolute bottom-[15%] left-0 w-1/5 max-w-[300px]" src="@/assets/images/hero-2.png" alt="Sakura" />
 		<div class="container mx-auto px-8 pt-36 pb-16 md:pt-60 md:pb-24">
 			<div class="mb-20 flex flex-row-reverse items-start justify-start md:mb-32">
 				<div class="relative hidden w-2/5 pt-[40%] lg:block">
-					<img
-						class="absolute top-[-10%] right-[80%] w-3/5"
-						src="@/assets/images/hero-gate-back.svg"
+					<img class="absolute top-[-10%] right-[80%] w-3/5" src="@/assets/images/hero-gate-back.svg"
 						alt="Gate Back Pattern" />
-					<div
-						class="absolute top-0 left-0 w-full rounded-full bg-zinc-800 pt-[100%]" />
-					<img
-						class="absolute bottom-[90%] right-[105%] w-1/5"
-						src="@/assets/images/hero-leave.png"
+					<div class="absolute top-0 left-0 w-full rounded-full bg-zinc-800 pt-[100%]" />
+					<img class="absolute bottom-[90%] right-[105%] w-1/5" src="@/assets/images/hero-leave.png"
 						alt="Leave" />
-					<img
-						class="absolute top-0 left-0 z-20 w-full"
-						src="@/assets/images/hero-gate.svg"
-						alt="Gate" />
+					<img class="absolute top-0 left-0 z-20 w-full" src="@/assets/images/hero-gate.svg" alt="Gate" />
 				</div>
 				<div class="w-100 relative lg:w-3/5">
 					<div class="mb-2 font-bold italic text-light">
@@ -35,7 +22,7 @@
 						<span class="text-primary">Twin</span>ning is winning!
 					</div>
 					<div class="flex flex-col items-center sm:flex-row">
-						<a  @click.prevent="mintNft"
+						<a @click.prevent="mintNft"
 							class="mb-4 w-60 border border-solid border-primary bg-primary px-9 py-5 text-center font-bold sm:mb-0 sm:mr-8 hover:cursor-pointer">
 							Mint Founder's NFT
 						</a>
@@ -69,19 +56,55 @@
 <script>
 import { generateProof } from '@/utils/merkle-proof'
 import whitelistAddresses from '@/assets/json/addresses.json'
+import { getParsedEthersError } from "@enzoferey/ethers-error-parser";
+import { capitalCase } from 'change-case'
 
 export default {
 	name: 'HeroOpSection',
 	methods: {
 		async mintNft() {
-			try{
+			try {
 				const proof = await generateProof(this.$wallet.account, whitelistAddresses)
 				const contract = await this.$wallet.getContract()
 				const price = await contract.price();
 				const response = await contract.whitelistMint(proof, { value: price })
-			} catch(e){
+				this.$toast.show(`You have claimed a founder's pass NFT, thanks for minting!`, {
+					variant: 'success',
+					action: {
+						text: 'Close',
+						onClick: (e, toastObject) => {
+							toastObject.goAway(0)
+						},
+					},
+				})
+			} catch (e) {
 				console.error("whitelistMint error")
-				console.error(e)
+				let message = "Something went wrong! "
+				
+				try {
+					const { context } = getParsedEthersError(e);
+					switch(context){
+						case "ALREADY_MINTED":
+						  message = "Looks like you've already minted. No use doing it again!"
+						  break;
+						default:
+							// do nothing
+					}
+
+				} catch (e) {
+					console.error("error parsing error")
+					console.debug(e)
+				}
+
+				this.$toast.error(message, {
+					variant: 'danger',
+					action: {
+						text: 'Close',
+						onClick: (e, toastObject) => {
+							toastObject.goAway(0)
+						},
+					},
+				})
 			}
 		}
 	}
