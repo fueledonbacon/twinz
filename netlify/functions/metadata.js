a2885df911b8161d7a65b92e69452e5f5433d941
Ryan Cwynar <ryan@fueledonbacon.com>
2022-10-01 09:59:43 -0500

    change the image to the video

--- netlify/functions/metadata.js
+++ netlify/functions/metadata.js
@@ -6,7 +6,7 @@ export const handler = async event => {
     let metadata = {
         name: `Twinz NFT Founder's Pass #${id}`,
         description: "TwinZ is a collection of 10,000 digital individuals living as NFTs 5 different societies with completely different visual atmospheres",
-        image: "https://twinznftfp.s3.amazonaws.com/TwinZNFTFoundersPass.gif"
+        image: "https://twinznftfp.s3.amazonaws.com/twinz-founder-pass-video.mp4"
     }
 
     if(id === undefined || parseInt(id) > 100 || parseInt(id) < 0 || Number.isNaN(parseInt(id)) ){
