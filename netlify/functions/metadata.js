5c504375c78b0e77d01068a0be4fa94a8fcbefe7
Ryan Cwynar <ryan@fueledonbacon.com>
2022-09-30 23:41:59 -0500

    when there's no id

--- netlify/functions/metadata.js
+++ netlify/functions/metadata.js
@@ -2,12 +2,16 @@ export const handler = async event => {
   try {
     const { path } = event
     const id = path.split('/')[4]
-    if(id === undefined) throw new Error("ID is undefined")
     let metadata = {
         name: `Twinz NFT Founder's Pass #${id}`,
         description: "TwinZ is a collection of 10,000 digital individuals living as NFTs 5 different societies with completely different visual atmospheres",
         image: "https://twinznftfp.s3.amazonaws.com/TwinZNFTFoundersPass.gif"
     }
+
+    if(id === undefined){
+        metdata.name =  `Twinz NFT Founder's Passes`
+    }
+
     return { statusCode: 200, body: JSON.stringify(metadata) }
 
   } catch (error) {
