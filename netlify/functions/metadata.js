34fe8e39811d1c63c39da68be9745511691fa7a5
Ryan Cwynar <ryan@fueledonbacon.com>
2022-09-30 23:37:13 -0500

    push metadata function

--- netlify/functions/metadata.js
+++ netlify/functions/metadata.js
@@ -1,6 +1,14 @@
 export const handler = async event => {
   try {
-    return { statusCode: 200, body: JSON.stringify({ event }) }
+    const { path } = event
+    const id = path.split('/')[4]
+    if(id === undefined) throw new Error("ID is undefined")
+    let metadata = {
+        name: `Twinz NFT #${id}`,
+        description: "TwinZ is a collection of 10,000 digital individuals living as NFTs 5 different societies with completely different visual atmospheres",
+        image: "https://twinznftfp.s3.amazonaws.com/TwinZNFTFoundersPass.gif"
+    }
+    return { statusCode: 200, body: JSON.stringify(metadata) }
 
   } catch (error) {
     return { statusCode: 500, body: JSON.stringify(error) }
