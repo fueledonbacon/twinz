ea788b2081d90c0552e03bd548b47aca9b816b64
Ryan Cwynar <ryan@fueledonbacon.com>
2022-09-30 23:39:45 -0500

    founders pass

--- netlify/functions/metadata.js
+++ netlify/functions/metadata.js
@@ -4,7 +4,7 @@ export const handler = async event => {
     const id = path.split('/')[4]
     if(id === undefined) throw new Error("ID is undefined")
     let metadata = {
-        name: `Twinz NFT #${id}`,
+        name: `Twinz NFT Founder's Pass #${id}`,
         description: "TwinZ is a collection of 10,000 digital individuals living as NFTs 5 different societies with completely different visual atmospheres",
         image: "https://twinznftfp.s3.amazonaws.com/TwinZNFTFoundersPass.gif"
     }
