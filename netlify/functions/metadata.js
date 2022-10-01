d3e719e580bc44bb89b651e70c600738dfb32f49
Ryan Cwynar <ryan@fueledonbacon.com>
2022-09-30 23:43:33 -0500

    id

--- netlify/functions/metadata.js
+++ netlify/functions/metadata.js
@@ -2,13 +2,14 @@ export const handler = async event => {
   try {
     const { path } = event
     const id = path.split('/')[4]
+
     let metadata = {
         name: `Twinz NFT Founder's Pass #${id}`,
         description: "TwinZ is a collection of 10,000 digital individuals living as NFTs 5 different societies with completely different visual atmospheres",
         image: "https://twinznftfp.s3.amazonaws.com/TwinZNFTFoundersPass.gif"
     }
 
-    if(id === undefined){
+    if(id === undefined || parseInt(id) > 100){
         metdata.name =  `Twinz NFT Founder's Passes`
     }
 
