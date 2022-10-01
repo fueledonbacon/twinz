e1990a36bc9ea9a983ad2f2d0df09192622ac5b4
Ryan Cwynar <ryan@fueledonbacon.com>
2022-10-01 03:25:21 -0500

    match sorting algorithm to contract launch

--- utils/merkle-proof.js
+++ utils/merkle-proof.js
@@ -3,7 +3,7 @@ import { ethers } from 'ethers';
 
 const generateProof =  async (address, list) => {
     const leaves = list.map(x => ethers.utils.keccak256(x))
-    const tree = new MerkleTree(leaves, ethers.utils.keccak256 , { sort: true });
+    const tree = new MerkleTree(leaves, ethers.utils.keccak256 , { sortPairs: true });
     console.debug(tree.getHexRoot());
     const leaf = await ethers.utils.keccak256(address)
 
